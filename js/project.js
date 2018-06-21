b0ef13cdf0e60b6e6994ddb56238e3e64fee057d
dylanburkey <dylanburkey@outlook.com>
2018-06-21 14:27:10 -0400

    Updated for tonight.

---
+++ js/project.js
@@ -0,0 +1,222 @@
+// // Get new js calls
+let meet = {
+    origin: [],
+    destination: [],
+    findMiddle: function () {
+        // We are flat Earthers
+        this.theMiddle.lat = (this.origin[0] + this.destination[0]) / 2;
+        this.theMiddle.lng = (this.origin[1] + this.destination[1]) / 2;
+    },
+    theMiddle: {},
+}
+let origin = {};
+let destination = {};
+let theMiddle = {};
+let activity = "";
+let radius = 0;
+let map;
+
+function initMap() {
+    var map = new google.maps.Map(document.getElementById('map'), {
+        mapTypeControl: false,
+        center: { lat: -33.8688, lng: 151.2195 },
+        zoom: 13
+    });
+
+    new AutocompleteDirectionsHandler(map);
+}
+
+function AutocompleteDirectionsHandler(map) {
+    this.map = map;
+    this.originPlaceId = null;
+    this.destinationPlaceId = null;
+    this.travelMode = 'WALKING';
+    var originInput = document.getElementById('origin-input');
+    var destinationInput = document.getElementById('destination-input');
+    var modeSelector = document.getElementById('mode-selector');
+    this.directionsService = new google.maps.DirectionsService;
+    this.directionsDisplay = new google.maps.DirectionsRenderer;
+    this.directionsDisplay.setMap(map);
+
+    var originAutocomplete = new google.maps.places.Autocomplete(
+        originInput, { placeIdOnly: true });
+    var destinationAutocomplete = new google.maps.places.Autocomplete(
+        destinationInput, { placeIdOnly: true });
+
+    this.setupClickListener('changemode-walking', 'WALKING');
+    this.setupClickListener('changemode-transit', 'TRANSIT');
+    this.setupClickListener('changemode-driving', 'DRIVING');
+
+    this.setupPlaceChangedListener(originAutocomplete, 'ORIG');
+    this.setupPlaceChangedListener(destinationAutocomplete, 'DEST');
+
+    this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(originInput);
+    this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(destinationInput);
+    this.map.controls[google.maps.ControlPosition.TOP_LEFT].push(modeSelector);
+}
+
+// Sets a listener on a radio button to change the filter type on Places
+// Autocomplete. ("This" is redefined by choosing a tavel mode.)
+AutocompleteDirectionsHandler.prototype.setupClickListener = function (id, mode) {
+    var radioButton = document.getElementById(id);
+    var me = this;
+    radioButton.addEventListener('click', function () {
+        me.travelMode = mode;
+        me.route();
+    });
+};
+// This "me" is redefined for the place selected. If not origin, it is the destination. This is function for destination.
+AutocompleteDirectionsHandler.prototype.setupPlaceChangedListener = function (autocomplete, mode) {
+    var me = this;
+    autocomplete.bindTo('bounds', this.map);
+    autocomplete.addListener('place_changed', function () {
+        var place = autocomplete.getPlace();
+        if (!place.place_id) {
+            window.alert("Please select an option from the dropdown list.");
+            return;
+        }
+        if (mode === 'ORIG') {
+            me.originPlaceId = place.place_id;
+        } else {
+            me.destinationPlaceId = place.place_id;
+        }
+        me.route();
+    });
+
+};
+
+AutocompleteDirectionsHandler.prototype.route = function () {
+    if (!this.originPlaceId || !this.destinationPlaceId) {
+        return;
+    }
+    var me = this;
+
+    this.directionsService.route({
+        origin: { 'placeId': this.originPlaceId },
+        destination: { 'placeId': this.destinationPlaceId },
+        travelMode: this.travelMode
+    }, function (response, status) {
+        if (status === 'OK') {
+            me.directionsDisplay.setDirections(response);
+        } else {
+            window.alert('Directions request failed due to ' + status);
+        }
+    });
+};
+
+// Create object to store location data and find midpoint
+function findMiddle() {
+    // We are flat Earthers
+    meet.theMiddle.lat = (origin.lat + destination.lat) / 2;
+    meet.theMiddle.lng = (origin.lng + destination.lng) / 2;
+}
+
+const googleAPIkey = "AIzaSyAZ73W29ubLVV9YoW1dsMyob-ZlEiZWoPs";
+
+// Create click event
+$("button").click(function () {
+    // Get user input and store
+    const myLoc = $("#origin-input").val();
+    const friendLoc = $("#destination-input").val();
+    activity = $("#activity").val();
+    radius = $("#radius").val();
+
+    let googleURL = "https://maps.googleapis.com/maps/api/geocode/json?" +
+        "address=" + myLoc +
+        "&key=" + googleAPIkey;
+    // console.log(URL);
+
+    // Make API request
+    var getorigin = $.ajax({
+        url: googleURL,
+        method: "GET",
+    }),
+
+        getdestination = getorigin.then(function (response) {
+            // console log response
+            console.log(response);
+
+            // Store locOne lat-lng coords
+            origin.lat = response.results["0"].geometry.location.lat;
+            origin.lng = response.results["0"].geometry.location.lng;
+
+            // Update googleURL for location two
+            let googleURL = "https://maps.googleapis.com/maps/api/geocode/json?" +
+                "address=" + friendLoc +
+                "&key=" + googleAPIkey;
+
+            // Make locTwo Ajax request
+            return $.ajax({
+                url: googleURL,
+                method: "GET",
+            });
+        });
+
+    getdestination.done(function (response) {
+        console.log(response);
+        destination.lat = response.results["0"].geometry.location.lat;
+        destination.lng = response.results["0"].geometry.location.lng;
+
+        findMiddle();
+
+        // Return yelp Ajax request
+        getYelpData();
+    });
+});
+
+// Create function to make Yelp Ajax request
+function getYelpData() {
+    jQuery.ajaxPrefilter(function (options) {
+        if (options.crossDomain && jQuery.support.cors) {
+            options.url = 'https://cors-anywhere.herokuapp.com/' + options.url;
+        }
+    });
+
+    const yelpURL = "https://api.yelp.com/v3/businesses/search?" +
+        "latitude=" + meet.theMiddle.lat +
+        "&longitude=" + meet.theMiddle.lng +
+        "&term=" + activity +
+        "&radius=" + radius * 1609;
+
+    // Make API call
+    $.ajax({
+        url: yelpURL,
+        crossDomain: true,
+        method: "GET",
+        beforeSend: function (xhr) {
+            xhr.setRequestHeader('Authorization', 'Bearer gfAAsdu8zgOMbEu_1uAb1fMN4JtX982WdDD6dNgnkhbt0u4-nHcuAiL0uSZgRKkG3F4_I9wNNzBsFpZUo3K9RLz4VYswcm9FI44bf4s2S7hg_a8eqPBKnmKbmfUbW3Yx');
+        }
+    }).then(function (response) {
+        // Log response
+        console.log(response);
+
+        const labels = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T"];
+        for (i in response.businesses) {
+            var lat = response.businesses[i].coordinates.latitude;
+            var lng = response.businesses[i].coordinates.longitude;
+            new google.maps.Marker({
+                position: { lat, lng },
+                map: map,
+                label: labels[i]
+            });
+
+            var name = response.businesses[i].name;
+            var rating = response.businesses[i].rating;
+            // Add yelp results to html
+            addYelpDiv(labels[i], name, rating);
+
+        }
+        // function to add yelp results to html
+        function addYelpDiv(label, name, rating) {
+            // Create new div
+            let newDiv = $("<div>");
+            // Add class to div
+            newDiv.attr("class", "yelp-result");
+            // Append div to main yelp div
+            $("#yelp").append(newDiv);
+            // Add text to html
+            newDiv.text(label + ". " + name);
+            newDiv.append($("<p>").text("Rating: " + rating));
+        }
+    });
+}
